59087963460eeade9b9c2b19b2d5f5301b737fdc
Romain Crestey <rcrestey@gmail.com>
2025-11-13 21:06:38 +0100

    now class with createSubscriber

--- src/lib/Clock.svelte
+++ src/lib/Clock.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	// import { now } from './now.svelte';
-	import { now } from './now.store';
+	import { now } from './now.svelte';
 
 	interface Props {
 		size?: number;
@@ -29,13 +28,9 @@
 
 	const { size = 100 }: Props = $props();
 
-	// const hours = $derived(now.value.getHours());
-	// const minutes = $derived(now.value.getMinutes());
-	// const seconds = $derived(now.value.getSeconds());
-
-	const hours = $derived($now.getHours());
-	const minutes = $derived($now.getMinutes());
-	const seconds = $derived($now.getSeconds());
+	const hours = $derived(now.value.getHours());
+	const minutes = $derived(now.value.getMinutes());
+	const seconds = $derived(now.value.getSeconds());
 
 	const radius = $derived(size / 2);
 	const origin = $derived(`${radius}px ${radius}px`);
