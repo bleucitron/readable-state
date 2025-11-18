2cdb1713fedbd28bfb50bce7e44980792d703833
Romain Crestey <rcrestey@gmail.com>
2025-11-18 23:22:11 +0100

    page 3

---
+++ src/routes/3/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import profile from '$lib/assets/profile.png';
+</script>
+
+<div>
+	<img src={profile} alt="romain" />
+</div>
+
+<style>
+	div {
+		width: 200px;
+		height: 200px;
+		border-radius: 50%;
+		corner-shape: squircle;
+		overflow: hidden;
+
+		img {
+			width: 100%;
+		}
+	}
+</style>
