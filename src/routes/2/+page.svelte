df2bda58c6fa50b7e58963889af19ea517e3aba5
Romain Crestey <rcrestey@gmail.com>
2025-11-19 07:19:37 +0100

    add nice fade in

--- src/routes/2/+page.svelte
+++ src/routes/2/+page.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
 	import Clock from '$lib/ref/Clock.svelte';
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
 
 	let step = $state(0);
+	let on = $state(false);
+
+	onMount(() => {
+		on = true;
+	});
 </script>
 
-<h1>
-	readable state
+{#if on}
+	<h1 in:fade={{ duration: 2000 }}>
+		readable state
 
-	{#if step > 0}
-		<div>
-			<Clock size={50} />
-		</div>
-	{/if}
-</h1>
+		{#if step > 0}
+			<div>
+				<Clock size={50} />
+			</div>
+		{/if}
+	</h1>
+{/if}
 
 <svelte:window
 	on:keyup={(e) => {
